1d81876cff7900e42dbb6a33c8565eeaf6d53015
Francesco <franz.boschini@gmail.com>
2017-12-01 16:02:41 +0100

    replace header image with new one

--- Lightning/WeatherPresenter.swift
+++ Lightning/WeatherPresenter.swift
@@ -10,16 +10,13 @@ class WeatherPresenter: WeatherInteractorDelegate {
 
     func present(weather: Weather) {
         let cityName = weather.cityName!
-
         let icon = getIcon(for: weather)
-
         let theme = weather.isDayTime() ? WeatherTheme.Day : WeatherTheme.Night
-
         let temperature = String(format: "%.1f", weather.temperature!) + Utils().symbolBasedOnLocalUnits()
-
         let description = weather.description!.capitalized
 
         let displayedWeather = DisplayedWeather(cityName: cityName, icon: icon, theme: theme, temperature: temperature, description: description)
+
         view.show(weather: displayedWeather)
     }
 
